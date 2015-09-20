1d37729938c4f31501922ad33c6c9b76b721e539
skparkes <bias.variance@gmail.com>
2015-09-19 23:16:03 -0400

    Show utilization of local spark cluster.
    Do this to demonstrate that we really can work with big data, it's not
    just a myth.

--- 01_import_data.py
+++ 01_import_data.py
@@ -22,15 +22,20 @@ from pyspark.sql import SQLContext
 # LIBRARIES, LOCATIONS, LITERALS, ETC. GO ABOVE HERE
 #==============================================================================
 
+superman = indyspark.SparkCluster(n_workers=3)
+superman.start_cluster()
 
-conf = SparkConf().setAppName('playground').setMaster('local[3]')
-conf = conf.set('spark.serializer', 'org.apache.spark.serializer.KryoSerializer')
-conf = conf.set('spark.driver.memory', '2g')
-conf = conf.set('spark.executor.memory', '2g')
-conf = conf.set('spark.python.worker.memory', '2g')
-conf = conf.set('spark.python.worker.reuse', 'false')
-conf = conf.set('spark.eventlog.enabled', 'true')
+
+conf = SparkConf().setAppName('playground').setMaster(superman.url_master)
+# conf = conf.set('spark.serializer', 'org.apache.spark.serializer.KryoSerializer')
+# conf = conf.set('spark.driver.memory', '2g')
+# conf = conf.set('spark.executor.memory', '2g')
+# conf = conf.set('spark.python.worker.memory', '2g')
+# conf = conf.set('spark.python.worker.reuse', 'false')
+# conf = conf.set('spark.eventlog.enabled', 'true')
 sc = SparkContext(conf=conf)
 sqlContext = SQLContext(sc)
 
 print(indyspark.import_utils.import_csv(sqlContext, r'W:\NWS\Australia_Rentals\005_Raw_Data\sample_submission.csv', {'bob': 2}).take(42))
+
+superman.stop_cluster()
