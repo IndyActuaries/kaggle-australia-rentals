d58f4c2db079bec82e61143549cf0369d3376f35
shea-parkes <shea.parkes@milliman.com>
2015-09-27 19:10:21 -0400

    Author wrapper tooling.
    Do this to keep all the various cluster/application settings fairly
    centralized.

---
+++ indyaus/__init__.py
@@ -0,0 +1,11 @@
+"""
+## CODE OWNERS: Shea Parkes
+
+### OBJECTIVE:
+  Make the ~public bits easier to reach.
+
+### DEVELOPER NOTES:
+  Should really just be imports.
+"""
+
+from indyaus.spark_launcher import get_spark_pieces
